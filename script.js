4d2ce3977ae0b39032f019a7880fe67fe98f58d3
Caio Zidany <titidetutu11@gmail.com>
2024-06-03 15:51:58 -0300

    versão_4.2.0

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 document.addEventListener("DOMContentLoaded", function() {
+    const toggleButton = document.querySelector('.toggle-button');
+    const navLinks = document.querySelector('nav ul');
+
+    toggleButton.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+    });
+
     // Função para rolar suavemente até a seção desejada
     function scrollToSection(sectionId) {
         const section = document.getElementById(sectionId);
@@ -14,6 +21,9 @@ document.addEventListener("DOMContentLoaded", function() {
             event.preventDefault(); // Evita o comportamento padrão de um link
             const sectionId = this.getAttribute('href').substring(1); // Obtém o ID da seção
             scrollToSection(sectionId); // Chama a função de rolar suavemente
+            if (window.innerWidth <= 768) { // Fecha o menu após clique no item (em telas pequenas)
+                navLinks.classList.remove('active');
+            }
         });
     });
 });
